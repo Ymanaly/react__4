7ae8e7f1ce4a5345cc89bd219bcaf6df14113432
Ymanaly <old.vai2004@gmail.com>
2022-12-21 05:53:40 +0600

    load

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React, { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  const[values,setValues]=useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+  })
+
+
+  const [submitted,setSubmitted]=useState(false);
+
+const handleFirstNameInputChange=(event)=>{
+  setValues({...values,firstName:event.target.value})
+}
+const handleLasttNameInputChange=(event)=>{
+  setValues({...values,lasttName:event.target.value})
+}
+const handleEmailInputChange=(event)=>{
+  setValues({...values,email:event.target.value})
+}
+const handleSubmit=(event)=>{
+  event.preventDefault();
+  setSubmitted(true)
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="form-container">
+      <form class="register-form" onSubmit={handleSubmit}>
+        {submitted ? <div className="success-message">Спасибо за регистрацию!!!</div>:null}
+        <input
+        onChange={handleFirstNameInputChange}
+          value={values.firstName}
+          className="form-field"
+          placeholder="First Name"
+          name="firstName"
+        />
+        
+        <input
+        onChange={handleLasttNameInputChange}
+         value={values.lastName}
+         className="form-fiedl"
+          placeholder="Last Name"
+          name="lastName"
+        />
+       
+        <input
+        onChange={handleEmailInputChange}
+         value={values.email}
+         className="form-fiedl"
+          placeholder="Email"
+          name="email"
+        />
+        
+        <button class="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
-
-export default App;
